ce69878572e67b2329dccb36b86950cc830c91e6
Nico <nazucca7@gmail.com>
2025-03-11 09:23:37 -0600

    Revert "Merge remote-tracking branch '3d_upgrade/models_3d' into rolling_gnss_port"
    This reverts commit 793771e4a61c17a5f335481117c0739a9ac4c5ce, reversing
    changes made to a555586f88179673f4d46df25726ba615ff69643.

--- fuse_constraints/test/cost_function_gtest.hpp
+++ fuse_constraints/test/cost_function_gtest.hpp
@@ -38,7 +38,6 @@
 
 #include <memory>
 #include <vector>
-#include <Eigen/Core>
 
 /**
  * @brief A helper function to compare a expected and actual cost function.
@@ -53,8 +52,7 @@
  */
 static void ExpectCostFunctionsAreEqual(
   const ceres::CostFunction & cost_function,
-  const ceres::CostFunction & actual_cost_function, 
-  const double tol = 1e-16)
+  const ceres::CostFunction & actual_cost_function)
 {
   EXPECT_EQ(cost_function.num_residuals(), actual_cost_function.num_residuals());
   const size_t num_residuals = cost_function.num_residuals();
@@ -70,22 +68,8 @@ static void ExpectCostFunctionsAreEqual(
   }
 
   std::unique_ptr<double[]> parameters(new double[num_parameters]);
-  if ((num_parameters == 7) && (parameter_block_sizes[0] == 3) &&
-    (parameter_block_sizes[1] == 4))
-  {
-    // Special case for parameters[1] as quaternion
-    for (size_t i = 0; i < 3; i++) {
-      parameters[i] = static_cast<double>(i) + 1.0;
-    }
-    Eigen::Quaterniond q = Eigen::Quaterniond::UnitRandom();
-    parameters[3] = q.w();
-    parameters[4] = q.x();
-    parameters[5] = q.y();
-    parameters[6] = q.z();
-  } else {
-    for (size_t i = 0; i < num_parameters; ++i) {
-      parameters[i] = static_cast<double>(i) + 1.0;
-    }
+  for (size_t i = 0; i < num_parameters; ++i) {
+    parameters[i] = static_cast<double>(i) + 1.0;
   }
 
   std::unique_ptr<double[]> residuals(new double[num_residuals]);
@@ -112,7 +96,7 @@ static void ExpectCostFunctionsAreEqual(
       parameter_blocks.get(), actual_residuals.get(),
       nullptr));
   for (size_t i = 0; i < num_residuals; ++i) {
-    EXPECT_NEAR(residuals[i], actual_residuals[i], tol) << "residual id: " << i;
+    EXPECT_DOUBLE_EQ(residuals[i], actual_residuals[i]) << "residual id: " << i;
   }
 
   EXPECT_TRUE(
@@ -124,11 +108,11 @@ static void ExpectCostFunctionsAreEqual(
       parameter_blocks.get(), actual_residuals.get(),
       actual_jacobian_blocks.get()));
   for (size_t i = 0; i < num_residuals; ++i) {
-    EXPECT_NEAR(residuals[i], actual_residuals[i], tol) << "residual : " << i;
+    EXPECT_DOUBLE_EQ(residuals[i], actual_residuals[i]) << "residual : " << i;
   }
 
   for (size_t i = 0; i < num_residuals * num_parameters; ++i) {
-    EXPECT_NEAR(jacobians[i], actual_jacobians[i], tol)
+    EXPECT_DOUBLE_EQ(jacobians[i], actual_jacobians[i])
       << "jacobian : " << i << " " << jacobians[i] << " " << actual_jacobians[i];
   }
 }
--- fuse_constraints/test/test_absolute_orientation_3d_stamped_euler_constraint.cpp
+++ fuse_constraints/test/test_absolute_orientation_3d_stamped_euler_constraint.cpp
@@ -2,7 +2,6 @@
  * Software License Agreement (BSD License)
  *
  *  Copyright (c) 2018, Locus Robotics
- *  Copyright (c) 2023, Giacomo Franchini
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -151,8 +150,7 @@ TEST(AbsoluteOrientation3DStampedEulerConstraint, OptimizationFull)
   ceres::Solve(options, &problem, &summary);
 
   // Check
-  Eigen::Quaterniond expected =
-    Eigen::AngleAxisd(0.5, Eigen::Vector3d::UnitZ()) *
+  Eigen::Quaterniond expected = Eigen::AngleAxisd(0.5, Eigen::Vector3d::UnitZ()) *
     Eigen::AngleAxisd(1.5, Eigen::Vector3d::UnitY()) *
     Eigen::AngleAxisd(1.0, Eigen::Vector3d::UnitX());
   EXPECT_NEAR(expected.w(), orientation_variable->w(), 5.0e-3);
@@ -231,8 +229,7 @@ TEST(AbsoluteOrientation3DStampedEulerConstraint, OptimizationPartial)
   ceres::Solve(options, &problem, &summary);
 
   // Check
-  Eigen::Quaterniond expected =
-    Eigen::AngleAxisd(0.5, Eigen::Vector3d::UnitZ()) *
+  Eigen::Quaterniond expected = Eigen::AngleAxisd(0.5, Eigen::Vector3d::UnitZ()) *
     Eigen::AngleAxisd(1.5, Eigen::Vector3d::UnitY()) *
     Eigen::AngleAxisd(1.0, Eigen::Vector3d::UnitX());
   EXPECT_NEAR(expected.w(), orientation_variable->w(), 5.0e-3);
